9b6fe04b6989a58b9f1b5564393807a08e00c44f
Anoop Pratap Singh <anoopp780@gmail.com>
2024-08-25 01:16:57 +0530

    Updated image paths for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'; // You can remove this if all styles are converted to Tailwind
+import './App.css';
 
 function App() {
   const [currentImage, setCurrentImage] = useState('img1.png');
@@ -21,7 +21,7 @@ function App() {
     circle.innerHTML = '';
     for (let i = 0; i < 20; i++) {
       const bubble = document.createElement('div');
-      bubble.className = 'bubble absolute rounded-full bg-white/10';
+      bubble.className = 'bubble';
       const size = Math.random() * 60 + 20;
       bubble.style.width = `${size}px`;
       bubble.style.height = `${size}px`;
@@ -44,66 +44,36 @@ function App() {
   };
 
   return (
-    <section className="relative w-full min-h-screen py-24 px-10 flex justify-between items-center bg-white">
-      <div className={`circle absolute top-0 left-0 w-full h-full ${circleColor} transition-all duration-500 ease-in-out overflow-hidden`} style={{ clipPath: 'circle(600px at right 800px)' }}></div>
-      <header className="absolute top-0 left-0 w-full py-5 px-24 flex justify-between items-center">
-        <a href="/">
-          <img src={`${process.env.PUBLIC_URL}/images/logo.png`} className="relative max-w-[80px]" alt="Starbucks Logo" />
-        </a>
-        <ul className="relative flex">
-          <li className="list-none">
-            <a href="/" className="text-gray-800 font-normal ml-10">Home</a>
-          </li>
-          <li className="list-none">
-            <a href="/" className="text-gray-800 font-normal ml-10">Menu</a>
-          </li>
-          <li className="list-none">
-            <a href="/" className="text-gray-800 font-normal ml-10">What's New</a>
-          </li>
-          <li className="list-none">
-            <a href="/" className="text-gray-800 font-normal ml-10">Contact</a>
-          </li>
+    <section>
+      <div className="circle" style={{ backgroundColor: circleColor }}></div>
+      <header>
+        <a href="/"><img src={`${process.env.PUBLIC_URL}/images/logo.png`} className="logo" alt="Starbucks Logo" /></a>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Menu</a></li>
+          <li><a href="/">What's New</a></li>
+          <li><a href="/">Contact</a></li>
         </ul>
       </header>
-      <div className="content relative w-full flex justify-between items-center">
-        <div className="textBox max-w-[600px]">
-          <h2 className="text-gray-800 text-4xl leading-[1.4em] font-medium">
-            It's not just Coffee<br />It's <span className="text-green-700 text-xl font-black">Starbucks</span>
-          </h2>
-          <p className="text-gray-800">Starbucks is a chain of coffee shops in the heart of the city. They've been around for over 200 years and continue to grow in popularity.</p>
-          <a href="/" className="inline-block mt-5 px-5 py-2.5 bg-green-700 text-white rounded-full font-medium tracking-wider">Learn More</a>
+      <div className="content">
+        <div className="textBox">
+          <h2>It's not just Coffee<br />It's <span>Starbucks</span></h2>
+          <p>Starbucks is a chain of coffee shops in the heart of the city. They've been around for over 200 years and continue to grow in popularity.</p>
+          <a href="/" className="button">Learn More</a>
         </div>
-        <div className="imgBox w-[600px] flex justify-end pr-12 mt-12 pb-7">
-          <img src={`${process.env.PUBLIC_URL}/images/drinks/${currentImage}`} className="starbucks max-w-[250px] sm:max-w-[300px] md:max-w-[340px] drop-shadow-[10px_10px_15px_rgba(0,0,0,0.4)] transition-filter duration-300 ease-in-out hover:drop-shadow-[15px_15px_20px_rgba(0,0,0,0.5)]" alt="Starbucks Drink" />
+        <div className="imgBox">
+        <img src={`${process.env.PUBLIC_URL}/images/drinks/${currentImage}`} className="starbucks" alt="Starbucks Drink" />
         </div>
       </div>
-      <ul className="thumb absolute left-1/2 bottom-5 transform -translate-x-1/2 flex">
-        <li className="list-none inline-block mx-5 cursor-pointer transition-transform duration-500 hover:-translate-y-[15px]">
-          <img src={`${process.env.PUBLIC_URL}/images/thumbs/thumb1.png`} onClick={() => imgSlider('img1.png', '#017143')} className="max-w-[60px]" alt="Thumb 1" />
-        </li>
-        <li className="list-none inline-block mx-5 cursor-pointer transition-transform duration-500 hover:-translate-y-[15px]">
-          <img src={`${process.env.PUBLIC_URL}/images/thumbs/thumb2.png`} onClick={() => imgSlider('img2.png', '#eb7495')} className="max-w-[60px]" alt="Thumb 2" />
-        </li>
-        <li className="list-none inline-block mx-5 cursor-pointer transition-transform duration-500 hover:-translate-y-[15px]">
-          <img src={`${process.env.PUBLIC_URL}/images/thumbs/thumb3.png`} onClick={() => imgSlider('img3.png', '#d752b1')} className="max-w-[60px]" alt="Thumb 3" />
-        </li>
+      <ul className="thumb">
+      <li><img src={`${process.env.PUBLIC_URL}/images/thumbs/thumb1.png`} onClick={() => imgSlider('img1.png', '#017143')} alt="Thumb 1" /></li>
+      <li><img src={`${process.env.PUBLIC_URL}/images/thumbs/thumb2.png`} onClick={() => imgSlider('img2.png', '#eb7495')} alt="Thumb 2" /></li>
+      <li><img src={`${process.env.PUBLIC_URL}/images/thumbs/thumb3.png`} onClick={() => imgSlider('img3.png', '#d752b1')} alt="Thumb 3" /></li>
       </ul>
-      <ul className="sci absolute top-1/2 right-7 transform -translate-y-1/2 flex flex-col justify-center items-center">
-        <li className="list-none">
-          <a href="/" className="inline-block my-1.5 transform scale-60 filter invert">
-            <img src={`${process.env.PUBLIC_URL}/images/icons/facebook.png`} alt="Facebook" />
-          </a>
-        </li>
-        <li className="list-none">
-          <a href="/" className="inline-block my-1.5 transform scale-60 filter invert">
-            <img src={`${process.env.PUBLIC_URL}/images/icons/twitter.png`} alt="Twitter" />
-          </a>
-        </li>
-        <li className="list-none">
-          <a href="/" className="inline-block my-1.5 transform scale-60 filter invert">
-            <img src={`${process.env.PUBLIC_URL}/images/icons/instagram.png`} alt="Instagram" />
-          </a>
-        </li>
+      <ul className="sci">
+      <li><a href="/"><img src={`${process.env.PUBLIC_URL}/images/icons/facebook.png`} alt="Facebook" /></a></li>
+      <li><a href="/"><img src={`${process.env.PUBLIC_URL}/images/icons/twitter.png`} alt="Twitter" /></a></li>
+      <li><a href="/"><img src={`${process.env.PUBLIC_URL}/images/icons/instagram.png`} alt="Instagram" /></a></li>
       </ul>
     </section>
   );
